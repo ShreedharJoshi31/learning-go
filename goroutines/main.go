b595e1fddb774b63ec5f3b7e9e21df9cc6f38eff
ShreedharJoshi31 <shreedharjoshi03@gmail.com>
2024-11-01 12:40:56 +0530

    goroutines

--- goroutines/main.go
+++ goroutines/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func printNumbers() {
+func printNumbers(wg *sync.WaitGroup) {
+	defer wg.Done()
 	for i := range 10 {
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 		fmt.Printf("%d ", i)
 	}
 }
 
 func main() {
-	go printNumbers()
+	var wg sync.WaitGroup
+	wg.Add(2) // Add how many goroutines we have to perform
+	go printNumbers(&wg)
+	go printNumbers(&wg)
+	wg.Wait() // This will wait until all the goroutines are executed
 }
